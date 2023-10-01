6da0c83f3be9cf2e6d71b03eef2a51a0b740b76d
BehzodHamroyev <behzodhamroyev007@gmail.com>
2023-10-02 00:47:49 +0500

    learn fixtures,beforeEach,stub,spy,clock and tick ...

--- cypress/e2e/location.cy.js
+++ cypress/e2e/location.cy.js
@@ -2,6 +2,7 @@
 
 describe("share location", () => {
   beforeEach(() => {
+    cy.clock();
     cy.fixture("user-location.json").as("userLoaction");
     cy.get("@userLoaction").then((fakePostion) => {
       cy.visit("/").then((win) => {
@@ -16,6 +17,9 @@ describe("share location", () => {
         cy.stub(win.navigator.clipboard, "writeText")
           .as("shareLocation")
           .resolves();
+
+        cy.spy(win.localStorage, "setItem").as("localStorage");
+        cy.spy(win.localStorage, "getItem").as("getlocalStorage");
       });
     });
   });
@@ -32,9 +36,26 @@ describe("share location", () => {
     cy.get('[data-cy="get-loc-btn"]').click();
     cy.get('[data-cy="share-loc-btn"]').click();
     cy.get("@shareLocation").should("have.been.called");
-    cy.get("@shareLocation").should(
-      "have.been.calledWithMatch",
-      new RegExp(`${43.5}.*${32}.*${encodeURI("Jafar")}`)
-    );
+    cy.get("@userLoaction").then((position) => {
+      const { latitude, longitude } = position.coords;
+
+      cy.get("@shareLocation").should(
+        "have.been.calledWithMatch",
+        new RegExp(`${latitude}.*${longitude}.*${encodeURI("Jafar")}`)
+      );
+
+      cy.get("@localStorage").should(
+        "have.been.calledWithMatch",
+        /Jafar/,
+        new RegExp(`${latitude}.*${longitude}.*${encodeURI("Jafar")}`)
+      );
+    });
+    cy.get("@localStorage").should("have.been.called");
+    cy.get('[data-cy="share-loc-btn"]').click();
+    cy.get("@getlocalStorage").should("have.been.called");
+    cy.get('[data-cy="info-message"]').should("be.visible");
+    cy.get('[data-cy="info-message"]').should("have.class", "visible");
+    cy.tick(2000); // shuncha vaqt kutish anglatadi buni ishlatish uchun cy.clock() yozilishi kerak va each ichida yozilishining sababi u oldindan yuklangan bo'ladi
+    cy.get('[data-cy="info-message"]').should("not.be.visible");
   });
 });
